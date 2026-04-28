b9eab3337548ebe6ccd122e20f321c7bceae79d2
SilasNS <silasschlax@gmail.com>
2026-04-28 14:43:36 -0400

    Add new methods and tests to board
    Add #in_bounds?, #piece_at(pos), and #update_board(from, to, color)

--- lib/chess/board.rb
+++ lib/chess/board.rb
@@ -2,7 +2,7 @@ require_relative 'piece'
 Dir[File.join(__dir__, './pieces/*.rb')].sort.each { |file| require_relative file }
 # Board class
 class Board
-  attr_reader :board
+  attr_accessor :board
 
   def initialize
     middle_board = Array.new(4) { Array.new(8, nil) }
@@ -13,6 +13,36 @@ class Board
       create_pawn_row('w', 6),
       create_init_row('w', 7)
     ]
+  end
+
+  def move_piece?(from, to, color)
+    return false unless in_bounds?(from) && in_bounds?(to)
+    piece = piece_at(from)
+    if piece.valid_move?(to)
+      new_space = piece_at(to)
+
+      return false if piece.color.nil? || piece.color != color
+      return true if new_space.nil? || piece.color != new_space.color
+    end
+    false
+  end
+
+  def update_board(from, to, color)
+    return false unless move_piece?(from, to, color)
+
+    piece = piece_at(from)
+    piece.position = to
+    @board[to[0]][to[1]] = piece
+    @board[from[0]][from[1]] = nil
+  end
+
+  def piece_at(pos)
+    row, col = pos
+    @board[row][col]
+  end
+
+  def in_bounds?(pos)
+    pos[0].between?(0, 7) && pos[1].between?(0, 7)
   end
 
   private
@@ -35,4 +65,12 @@ class Board
   end
 end
 
-Board.new
+# board = Board.new
+# p board.board[0][1]
+# p board.board[2][0]
+# p board.update_board([0, 1], [2, 0], 'b')
+# p board.move_piece?([2, 0], [4, 3], 'b')
+# # board.update_board([4, 3], [6, 4], 'b')
+# # p board.board[0][1]
+# # p board.board[2][0]
+# # p board.board[4][3]
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,29 +1,142 @@
 require_relative 'spec_helper'
 require_relative '../lib/chess/board'
+require_relative '../lib/chess/piece'
+require_relative '../lib/chess/pieces/pawn'
 
 RSpec.describe Board do
-  let(:board) { described_class.new }
-  context 'when board is initalized' do
-    it 'creates a 8x8 grid' do
-      expect(board.board.length).to be(8)
-      board.board.each do |row|
-        expect(row.length).to be(8)
+  describe '#initialize' do
+    let(:board) { described_class.new }
+    context 'when board is initalized' do
+      it 'creates a 8x8 grid' do
+        expect(board.board.length).to be(8)
+        board.board.each do |row|
+          expect(row.length).to be(8)
+        end
+      end
+      it 'fills row 6 with white pawns' do
+        expect(board.board[6].all? { |piece| piece.is_a?(Pawn) && piece.color == 'w' })
+      end
+      it 'fills row 1 with black pawns' do
+        expect(board.board[1].all? { |piece| piece.is_a?(Pawn) && piece.color == 'b' })
+      end
+      it 'fills row 7 with white peices' do
+        expect(board.board[7].none?(&:nil?)).to be true
+      end
+      it 'fills row 0 with black peices' do
+        expect(board.board[0].none?(&:nil?)).to be true
+      end
+      it 'sets rows 2-5 to nil' do
+        expect(board.board[2..5].none?(&:nil?)).to be true
       end
     end
-    it 'fills row 6 with white pawns' do
-      expect(board.board[6].all? { |piece| piece.is_a?(Pawn) && piece.color == 'w' })
+  end
+
+  describe '#in_bounds?' do
+    let(:board_in_bounds) { described_class.new }
+    context 'returns true' do
+      it 'when position is [0, 0]' do
+        expect(board_in_bounds.in_bounds?([0, 0])).to be true
+      end
+      it 'when position is [7, 7]' do
+        expect(board_in_bounds.in_bounds?([7, 7])).to be true
+      end
+      it 'when position is [5, 6]' do
+        expect(board_in_bounds.in_bounds?([5, 6])).to be true
+      end
     end
-    it 'fills row 1 with black pawns' do
-      expect(board.board[1].all? { |piece| piece.is_a?(Pawn) && piece.color == 'b' })
+    context 'returns false' do
+      it 'when position is [-1, 0]' do
+        expect(board_in_bounds.in_bounds?([-1, 0])).to be false
+      end
+      it 'when position is [8, 8]' do
+        expect(board_in_bounds.in_bounds?([8, 8])).to be false
+      end
     end
-    it 'fills row 7 with white peices' do
-      expect(board.board[7].none?(&:nil?)).to be true
+  end
+
+  describe '#piece_at' do
+    let(:piece) { described_class.new }
+    context 'returns peice' do
+      it 'when position is [0, 0]' do
+        expect(piece.piece_at([0, 1])).to be_a(Knight)
+      end
+      it 'when position is [0, 5]' do
+        expect(piece.piece_at([0, 5])).to be_a(Bishop)
+      end
     end
-    it 'fills row 0 with black peices' do
-      expect(board.board[0].none?(&:nil?)).to be true
+    context 'rerns nil' do
+      it 'when positon is [3, 5]' do
+        expect(piece.piece_at([3, 5])).to be nil
+      end
+      it 'when position is [4, 5]' do
+        expect(piece.piece_at([4, 5])).to be nil
+      end
     end
-    it 'sets rows 2-5 to nil' do
-      expect(board.board[2..5].none?(&:nil?)).to be true
+  end
+
+  describe '#move_piece?' do
+    let(:move) { described_class.new }
+    context 'returns true when piece is knight, starting at [0, 1]' do
+      it 'moving to [2, 0]' do
+        expect(move.move_piece?([0, 1], [2, 0], 'b')).to be true
+      end
+      it 'moving to [2, 2]' do
+        expect(move.move_piece?([0, 1], [2, 2], 'b')).to be true
+      end
+    end
+    context 'returns false when piece is knight, starting at [0, 1]' do
+      it 'moving to [-2, 0]' do
+        expect(move.move_piece?([0, 1], [-2, 0], 'b')).to be false
+      end
+      it 'moving to [0, 0]' do
+        expect(move.move_piece?([0, 1], [0, 0], 'b')).to be false
+      end
+    end
+  end
+
+  describe '#update_board' do
+    let(:board) { described_class.new }
+    context 'updated the board' do
+      before do
+        board.update_board([0, 1], [2, 0], 'b')
+      end
+      it 'when move is valid' do
+        expect(board.board[0][1]).to be nil
+        expect(board.board[2][0]).to be_a(Knight)
+      end
+    end
+    context 'update the board' do
+      before do
+        board.update_board([0, 1], [2, 2], 'b')
+        board.update_board([2, 2], [4, 3], 'b')
+        board.update_board([4, 3], [6, 4], 'b')
+      end
+      it 'when move is valid and capture' do
+        expect(board.board[0][1]).to be nil
+        expect(board.board[2][2]).to be nil
+        expect(board.board[4][3]).to be nil
+        expect(board.board[6][4]).to be_a(Knight)
+      end
+    end
+    context 'does not update the board' do
+      before do
+        board.update_board([0, 1], [1, 0], 'b')
+      end
+      it 'when move is invalid' do
+        expect(board.board[0][1]).to be_a(Knight)
+        expect(board.board[1][0]).to be_a(Pawn)
+      end
+    end
+    context 'does not update the board' do
+      before do
+        board.update_board([0, 1], [2, 2], 'b')
+        board.update_board([2, 2], [1, 4], 'b')
+      end
+      it 'when move is capturing players own piece' do
+        expect(board.board[0][1]).to be nil
+        expect(board.board[2][2]).to be_a(Knight)
+        expect(board.board[1][4]).to be_a(Pawn)
+      end
     end
   end
 end
