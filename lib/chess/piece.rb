74ade48dcd63c65db8beb35169ff489032e0ea36
SilasNS <silasschlax@gmail.com>
2026-04-28 10:29:32 -0400

    Create Piece class with simple tests

---
+++ lib/chess/piece.rb
@@ -0,0 +1,9 @@
+# Piece class
+class Piece
+  attr_reader :color, :position
+
+  def initialize(color, position)
+    @color = color
+    @position = position
+  end
+end
---
+++ spec/piece_spec.rb
@@ -0,0 +1,14 @@
+require_relative 'spec_helper'
+require_relative '../lib/chess/piece'
+
+RSpec.describe Piece do
+  context 'when a piece is created' do
+    let(:piece) { described_class.new('w', [0, 1]) }
+    it 'has color' do
+      expect(piece.color).to eq('w')
+    end
+    it 'has position' do
+      expect(piece.position).to eq([0, 1])
+    end
+  end
+end
