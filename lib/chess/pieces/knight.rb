dd68af5e71e80c248f83b5a6f94dcdead0f36379
SilasNS <silasschlax@gmail.com>
2026-04-28 12:59:02 -0400

    Add #valid_moves? for Knight with tests

--- lib/chess/pieces/knight.rb
+++ lib/chess/pieces/knight.rb
@@ -1,5 +1,17 @@
 require_relative '../piece'
 # Pawn class
 class Knight < Piece
+  DIRECTIONS = [
+    [-2, -1], [-2, 1], [-1, 2], [1, 2], [2, 1], [2, -1], [1, -2], [-1, -2]
+  ].freeze
 
+  def valid_move?(from, to)
+    possible_moves = []
+    DIRECTIONS.map do |dir|
+      possible_moves.push([from[0] + dir[0], from[1] + dir[1]])
+    end
+    return true if possible_moves.include?(to)
+
+    false
+  end
 end
--- spec/pieces/knight_spec.rb
+++ spec/pieces/knight_spec.rb
@@ -2,13 +2,30 @@ require_relative '../spec_helper'
 require_relative '../../lib/chess/pieces/knight'
 
 RSpec.describe Knight do
+  let(:knight) { described_class.new('w', [1, 2]) }
   context 'when peice is created' do
-    let(:knight) { described_class.new('w', [1, 2]) }
     it 'has a color' do
       expect(knight.color).to eq('w')
     end
     it 'has a position' do
       expect(knight.position).to eq([1, 2])
+    end
+  end
+
+  describe '#valid_move' do
+    let(:from) { [0, 1] }
+    context 'returns true' do
+      it 'when space is [2, 0]' do
+        expect(knight.valid_move?(from, [2, 0])).to be true
+      end
+      it 'when space is [2, 2]' do
+        expect(knight.valid_move?(from, [2, 2])).to be true
+      end
+    end
+    context 'returns false' do
+      it 'when space is [0, 0]' do
+        expect(knight.valid_move?(from, [0, 0])).to be false
+      end
     end
   end
 end
