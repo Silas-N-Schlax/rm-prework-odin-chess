bcd8066ad4fa474df575d605077e55a5010fb09d
SilasNS <silasschlax@gmail.com>
2026-04-28 18:08:08 -0400

    Add Rook movement checks and tests
    Bounds not tested as Board class tests before calling.

--- lib/chess/pieces/rook.rb
+++ lib/chess/pieces/rook.rb
@@ -1,5 +1,24 @@
 require_relative '../piece'
-# Pawn class
+# Rook class
 class Rook < Piece
+  DIRECTIONS = [
+    [0, 1], [1, 0], [0, -1], [-1, 0]
+  ].freeze
 
+  def valid_move?(board, to, pos = @position)
+    dir_x = to[1] - pos[1]
+    dir_y = to[0] - pos[0]
+    return false unless dir_x.zero? || dir_y.zero?
+
+    y, x = pos
+    loop do
+      x += dir_x <=> 0
+      y += dir_y <=> 0
+      break if to == [y, x]
+
+      return false unless board.piece_at([y, x]).nil?
+    end
+    target = board.piece_at(to)
+    target.nil? || target.color != @color
+  end
 end
--- spec/pieces/rook_spec.rb
+++ spec/pieces/rook_spec.rb
@@ -1,14 +1,45 @@
 require_relative '../spec_helper'
 require_relative '../../lib/chess/pieces/rook'
+require_relative '../../lib/chess/board'
 
 RSpec.describe Rook do
   context 'when peice is created' do
-    let(:rook) { described_class.new('w', [1, 2]) }
+    let(:rook) { described_class.new('b', [0, 0]) }
     it 'has a color' do
-      expect(rook.color).to eq('w')
+      expect(rook.color).to eq('b')
     end
     it 'has a position' do
-      expect(rook.position).to eq([1, 2])
+      expect(rook.position).to eq([0, 0])
+    end
+  end
+  describe '#valid_move' do
+    let(:board) { Board.new }
+    let(:rook) { described_class.new('b', [0, 0]) }
+    context 'returns true' do
+      it 'when space is [0, 1]' do
+        board.board[0][1] = nil
+        expect(rook.valid_move?(board, [0, 1])).to be true
+      end
+      it 'when space is [4, 0]' do
+        board.board[1][0] = nil
+        expect(rook.valid_move?(board, [4, 0])).to be true
+      end
+      it 'when space is [4, 0] and a capture' do
+        board.board[1][0] = nil
+        board.board[4][0] = Rook.new('w', [4, 0])
+        expect(rook.valid_move?(board, [4, 0])).to be true
+      end
+    end
+    context 'returns false' do
+      it 'when space is [0, 1] and collision' do
+        expect(rook.valid_move?(board, [0, 1])).to be false
+      end
+      it 'when space is [4, 0] and collision' do
+        expect(rook.valid_move?(board, [4, 0])).to be false
+      end
+      it 'when space is [4, 0] and a friendly piece' do
+        expect(rook.valid_move?(board, [4, 0])).to be false
+      end
     end
   end
 end
