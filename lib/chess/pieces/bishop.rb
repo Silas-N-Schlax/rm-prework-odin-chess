5a756b1584e5693a3db973743af748fd1bc0b001
SilasNS <silasschlax@gmail.com>
2026-04-28 18:25:44 -0400

    Add Biship movement checks and tests
    Bounds not tested as Board class tests before calling.

--- lib/chess/pieces/bishop.rb
+++ lib/chess/pieces/bishop.rb
@@ -1,5 +1,24 @@
 require_relative '../piece'
-# Pawn class
+# Bishop class
 class Bishop < Piece
+  DIRECTIONS = [
+    [1, 1], [1, -1], [-1, -1], [-1, 1]
+  ].freeze
 
+  def valid_move?(board, to, pos = @position)
+    dir_x = to[1] - pos[1]
+    dir_y = to[0] - pos[0]
+    return false unless dir_x.abs == dir_y.abs
+
+    y, x = pos
+    loop do
+      x += dir_x <=> 0
+      y += dir_y <=> 0
+      break if to == [y, x]
+
+      return false unless board.piece_at([y, x]).nil?
+    end
+    target = board.piece_at(to)
+    target.nil? || target.color != @color
+  end
 end
--- spec/pieces/bishop_spec.rb
+++ spec/pieces/bishop_spec.rb
@@ -1,5 +1,6 @@
 require_relative '../spec_helper'
 require_relative '../../lib/chess/pieces/bishop'
+require_relative '../../lib/chess/board'
 
 RSpec.describe Bishop do
   context 'when peice is created' do
@@ -9,6 +10,35 @@ RSpec.describe Bishop do
     end
     it 'has a position' do
       expect(bishop.position).to eq([1, 2])
+    end
+  end
+
+  describe '#valid_move' do
+    let(:board) { Board.new }
+    let(:rook) { described_class.new('b', [0, 2]) }
+    context 'returns true' do
+      it 'when space is [2, 0]' do
+        board.board[1][1] = nil
+        expect(rook.valid_move?(board, [2, 0])).to be true
+      end
+      it 'when space is [2, 4]' do
+        board.board[1][3] = nil
+        expect(rook.valid_move?(board, [2, 4])).to be true
+      end
+    end
+    context 'returns false' do
+      it 'when space is [3, 0] and collision' do
+        expect(rook.valid_move?(board, [3, 0])).to be false
+      end
+      it 'when space is [3, 3] and collision' do
+        expect(rook.valid_move?(board, [3, 3])).to be false
+      end
+      it 'when space is [1, 1] and a friendly piece' do
+        expect(rook.valid_move?(board, [1, 1])).to be false
+      end
+      it 'when space is [3, 2] and a is not a diagonal' do
+        expect(rook.valid_move?(board, [3, 2])).to be false
+      end
     end
   end
 end
