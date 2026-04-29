39aa8de0b3960c265854196ade39a14b21911212
SilasNS <silasschlax@gmail.com>
2026-04-29 14:01:18 -0400

    Add grid cords to UI to help users

--- lib/chess/board/send_board.rb
+++ lib/chess/board/send_board.rb
@@ -1,7 +1,9 @@
 # Send board module
 module SendBoard
   def send_board
+    puts '   1  2  3  4  5  6  7  8'
     puts generate_board
+    puts '   1  2  3  4  5  6  7  8'
   end
 
   def generate_board
@@ -14,9 +16,9 @@ module SendBoard
 
   def board_row(row, index)
     if index.even?
-      "\e[40m #{piece(row[0])} \e[47m\e[30m #{piece(row[1])} \e[40m #{piece(row[2])} \e[47m\e[30m #{piece(row[3])} \e[40m #{piece(row[4])} \e[47m\e[30m #{piece(row[5])} \e[40m #{piece(row[6])} \e[47m\e[30m #{piece(row[7])} \e[0m"
+      "#{index + 1} \e[40m #{piece(row[0])} \e[47m\e[30m #{piece(row[1])} \e[40m #{piece(row[2])} \e[47m\e[30m #{piece(row[3])} \e[40m #{piece(row[4])} \e[47m\e[30m #{piece(row[5])} \e[40m #{piece(row[6])} \e[47m\e[30m #{piece(row[7])} \e[0m #{index + 1}"
     else
-      "\e[47m\e[30m #{piece(row[0])} \e[40m #{piece(row[1])} \e[47m\e[30m #{piece(row[2])} \e[40m #{piece(row[3])} \e[47m\e[30m #{piece(row[4])} \e[40m #{piece(row[5])} \e[47m\e[30m #{piece(row[6])} \e[40m #{piece(row[7])} \e[0m"
+      "#{index + 1} \e[47m\e[30m #{piece(row[0])} \e[40m #{piece(row[1])} \e[47m\e[30m #{piece(row[2])} \e[40m #{piece(row[3])} \e[47m\e[30m #{piece(row[4])} \e[40m #{piece(row[5])} \e[47m\e[30m #{piece(row[6])} \e[40m #{piece(row[7])} \e[0m #{index + 1}"
     end
   end
 
