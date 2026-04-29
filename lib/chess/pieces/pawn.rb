b7ca9e92474c3b1c3f30be5676750e6b9ff7f585
SilasNS <silasschlax@gmail.com>
2026-04-28 20:34:55 -0400

    Add Pawn movement checks and tests
    Bounds not tested as Board class tests before calling. Check for
    capture on the diagonal, and non caputure allowed on forward movement.

--- lib/chess/pieces/pawn.rb
+++ lib/chess/pieces/pawn.rb
@@ -1,5 +1,43 @@
 require_relative '../piece'
 # Pawn class
 class Pawn < Piece
+  def valid_move?(board, to, pos = @position)
+    dir_x = to[1] - pos[1]
+    dir_y = to[0] - pos[0]
+    y_step = @color == 'b' ? 1 : -1
+    return false if dir_y > 2 || dir_y < -2
+    return capture?(board, to, y_step, pos) unless dir_x.zero?
 
+    return false if (dir_y.negative? && !y_step.negative?) || (!dir_y.negative? && y_step.negative?)
+    y, x = pos
+    if dir_y == y_step
+      return true if board.piece_at([y + y_step, x]).nil?
+    elsif starting_position?
+      return true if board.piece_at([y + y_step, x]).nil? && board.piece_at([(y + (y_step * 2)), x]).nil?
+    end
+    false
+  end
+
+  def starting_position?
+    return true if @position[0] == 1 && @color == 'b'
+    return true if @position[0] == 6 && @color == 'w'
+
+    false
+  end
+
+  def capture?(board, to, y_step, pos = @position)
+    dir_x = to[1] - pos[1]
+    dir_y = to[0] - pos[0]
+    return false unless dir_x.abs == 1 && dir_y == y_step
+
+    y, x = pos
+    x += dir_x <=> 0
+    y += dir_y <=> 0
+    return false unless board.piece_at([y, x])
+
+    target = board.piece_at(to)
+    return true if target.nil? || target.color != @color
+
+    false
+  end
 end
--- spec/pieces/pawn_spec.rb
+++ spec/pieces/pawn_spec.rb
@@ -1,5 +1,6 @@
 require_relative '../spec_helper'
 require_relative '../../lib/chess/pieces/pawn'
+require_relative '../../lib/chess/board'
 
 RSpec.describe Pawn do
   context 'when peice is created' do
@@ -9,6 +10,121 @@ RSpec.describe Pawn do
     end
     it 'has a position' do
       expect(pawn.position).to eq([1, 2])
+    end
+  end
+
+  describe '#valid_move' do
+    let(:board) { Board.new }
+    context 'when black pawn moves' do
+      let(:rook) { described_class.new('b', [1, 0]) }
+      context 'returns true' do
+        it 'when space is [2, 0]' do
+          expect(rook.valid_move?(board, [2, 0])).to be true
+        end
+        it 'when space is [3, 0] and pawn is in starting position' do
+          expect(rook.valid_move?(board, [3, 0])).to be true
+        end
+        it 'when space is [2, 1] and capture' do
+          board.board[2][1] = Pawn.new('w', [2, 1])
+          expect(rook.valid_move?(board, [2, 1])).to be true
+        end
+      end
+      context 'returns false' do
+        it 'when space is [0, 0]' do
+          expect(rook.valid_move?(board, [0, 0])).to be false
+        end
+        it 'when space is [0, 0] and rook has been removed' do
+          board.board[0][0] = nil
+          expect(rook.valid_move?(board, [0, 0])).to be false
+        end
+        it 'when space is [4, 0]' do
+          expect(rook.valid_move?(board, [4, 0])).to be false
+        end
+        it 'when space is [2, 1] and no capture' do
+          expect(rook.valid_move?(board, [2, 1])).to be false
+        end
+        it 'when space is [2, 1] and peice is friendly' do
+          board.board[2][1] = Pawn.new('b', [2, 1])
+          expect(rook.valid_move?(board, [2, 1])).to be false
+        end
+        it 'when space is [3, 2] (out of bound capture attempt)' do
+          expect(rook.valid_move?(board, [3, 2])).to be false
+        end
+        it 'when space is [0, 1]' do
+          expect(rook.valid_move?(board, [0, 1])).to be false
+        end
+        it 'when space is [0, 1] and space is empty' do
+          board.board[0][1] = nil
+          expect(rook.valid_move?(board, [0, 1])).to be false
+        end
+        it 'when space is [3, 2] and peice in between' do
+          board.board[2][1] = Pawn.new('w', [2, 1])
+          expect(rook.valid_move?(board, [3, 2])).to be false
+        end
+        it 'when space is [0, 1] and enemy peice' do
+          board.board[0][1] = Pawn.new('w', [0, 1])
+          expect(rook.valid_move?(board, [0, 1])).to be false
+        end
+        it 'when space is [1, 2]' do
+          expect(rook.valid_move?(board, [1, 2])).to be false
+        end
+        it 'when space is [1, 2] and space is empty' do
+          board.board[1][2] = nil
+          expect(rook.valid_move?(board, [1, 2])).to be false
+        end
+      end
+    end
+    context 'when white pawn moves' do
+      let(:rook) { described_class.new('w', [6, 0]) }
+      context 'returns true' do
+        it 'when space is [5, 0]' do
+          expect(rook.valid_move?(board, [5, 0])).to be true
+        end
+        it 'when space is [4, 0] and pawn is in starting position' do
+          expect(rook.valid_move?(board, [4, 0])).to be true
+        end
+        it 'when space is [5, 1] and capture' do
+          board.board[5][1] = Pawn.new('b', [5, 1])
+          expect(rook.valid_move?(board, [5, 1])).to be true
+        end
+      end
+      context 'returns false' do
+        it 'when space is [7, 0]' do
+          expect(rook.valid_move?(board, [7, 0])).to be false
+        end
+        it 'when space is [7, 0] and rook has been removed' do
+          board.board[7][0] = nil
+          expect(rook.valid_move?(board, [7, 0])).to be false
+        end
+        it 'when space is [3, 0]' do
+          expect(rook.valid_move?(board, [3, 0])).to be false
+        end
+        it 'when space is [5, 1] and no capture' do
+          expect(rook.valid_move?(board, [5, 1])).to be false
+        end
+        it 'when space is [4, 2] (out of bound capture attempt)' do
+          expect(rook.valid_move?(board, [4, 2])).to be false
+        end
+        it 'when space is [5, 1] and peice is friendly' do
+          board.board[5][1] = Pawn.new('w', [5, 1])
+          expect(rook.valid_move?(board, [5, 1])).to be false
+        end
+        it 'when space is [7, 1]' do
+          expect(rook.valid_move?(board, [7, 1])).to be false
+        end
+        it 'when space is [0, 1] and space is empty' do
+          board.board[7][1] = nil
+          expect(rook.valid_move?(board, [7, 1])).to be false
+        end
+        it 'when space is [4, 2] and peice in between' do
+          board.board[3][1] = Pawn.new('w', [3, 1])
+          expect(rook.valid_move?(board, [4, 2])).to be false
+        end
+        it 'when space is [7, 1] and enemy peice' do
+          board.board[7][1] = Pawn.new('b', [7, 1])
+          expect(rook.valid_move?(board, [7, 1])).to be false
+        end
+      end
     end
   end
 end
