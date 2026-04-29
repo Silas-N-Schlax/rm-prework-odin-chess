8a80499610e903ce9f5b64ac77cd8d8e51f57520
SilasNS <silasschlax@gmail.com>
2026-04-29 11:42:57 -0400

    Add new test for bp

--- spec/piece_spec.rb
+++ spec/piece_spec.rb
@@ -3,12 +3,15 @@ require_relative '../lib/chess/piece'
 
 RSpec.describe Piece do
   context 'when a piece is created' do
-    let(:piece) { described_class.new('w', [0, 1], '⚠️') }
+    let(:piece) { described_class.new('w', [0, 1], '♖') }
     it 'has color' do
       expect(piece.color).to eq('w')
     end
     it 'has position' do
       expect(piece.position).to eq([0, 1])
+    end
+    it 'has white rook as peice' do
+      expect(piece.bp).to eq('♖')
     end
   end
 end
