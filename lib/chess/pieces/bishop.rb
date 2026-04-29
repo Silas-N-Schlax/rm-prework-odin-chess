e02657689b5fe5d0382b264bf6aff975cba4faa6
SilasNS <silasschlax@gmail.com>
2026-04-28 20:38:53 -0400

    Move Bishop and Rook movement to Piece class
    This allows the Queen to also access these checks

--- lib/chess/pieces/bishop.rb
+++ lib/chess/pieces/bishop.rb
@@ -6,19 +6,6 @@ class Bishop < Piece
   ].freeze
 
   def valid_move?(board, to, pos = @position)
-    dir_x = to[1] - pos[1]
-    dir_y = to[0] - pos[0]
-    return false unless dir_x.abs == dir_y.abs
-
-    y, x = pos
-    loop do
-      x += dir_x <=> 0
-      y += dir_y <=> 0
-      break if to == [y, x]
-
-      return false unless board.piece_at([y, x]).nil?
-    end
-    target = board.piece_at(to)
-    target.nil? || target.color != @color
+    diagonal_movement(board, to, pos)
   end
 end
